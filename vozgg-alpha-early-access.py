87050915f284bb921da2bc62eeddfcd5aeab9489
AyuVoZ <48995999+AyuVoZ@users.noreply.github.com>
2019-03-29 12:07:36 +0100

    Update vozgg-alpha-early-access.py
    ajout son

--- vozgg-alpha-early-access.py
+++ vozgg-alpha-early-access.py
@@ -1,25 +1,26 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Mar 28 14:10:51 2019
-
-@author: gwend,Nwen
+Created on Wed Mar 27 16:50:57 2019
+@author: nwenn
 """
+
 import random, pygame, os
 #from pygame.locals import *
 
 pygame.init()
 pygame.display.set_caption("VoZ.GG alpha early access")
 
-fenetre = pygame.display.set_mode((1540,794))
+fenetre = pygame.display.set_mode((1080,720))
 clock = pygame.time.Clock()
 
 running = True
 bg = pygame.image.load("bg.jpg").convert()
-bg = pygame.transform.scale(bg,(1540,794))
+bg = pygame.transform.scale(bg,(1080,720))
 fenetre.blit(bg,(0,0))
 
-img = pygame.image.load("10E.jpg").convert()
-imgrect = img.get_rect()
+'''img = pygame.image.load("10E.jpg").convert()
+imgrect = img.get_rect()'''
+
 
 class Player(object):
     
@@ -54,19 +55,29 @@ class Player(object):
                     
 class Wall(object):
     
-    def __init__(self, pos):
+    def __init__(self, pos, size):
         walls.append(self)
-        self.rect = pygame.Rect(pos[0], pos[1], 32, 32)
+        self.rect = pygame.Rect(pos[0], pos[1], size[0], size[1])
         
+def Gravity(p):
+    p.move(0,1)
+
+leSol = pygame.Rect(0,620,1080,100)
+
 coord=[(500,500),(400,300),(1000,250),(720,500),(592,600),(100,100)]
-walls=[]        
+walls=[]   
+leSol=Wall((0,620),(1080,100))
+wallSize=(50,50)
 i = 0
 for e in coord :
-    walls.append(Wall(e))
+    walls.append(Wall(e,wallSize))
     i+=1
+pygame.mixer.music.load('son.mp3')
 
 player = Player()
 
+
+    
 while running :
     clock.tick(60)
     for e in pygame.event.get():
@@ -76,6 +87,9 @@ while running :
             running = False
     
     # Move the player if an arrow key is pressed
+    
+    
+    Gravity(player)
     key = pygame.key.get_pressed()
     if key[pygame.K_LEFT]:
         player.move(-2, 0)
@@ -87,10 +101,14 @@ while running :
         player.move(0, 2)
     if key[pygame.K_c]:
         fenetre.blit(bg,(0,0))
-    
+    if key[pygame.K_x]:
+        pygame.mixer.music.play(0)
+
     for wall in walls:
-        pygame.draw.rect(fenetre, (255, 255, 255), wall.rect)
-        
-    pygame.draw.rect(fenetre, (255, 200, 0), player.rect)
+        pygame.draw.rect(fenetre, (255, 0, 0), wall.rect)
     
+    pygame.draw.rect(fenetre, (255, 200, 0), player.rect)
+
     pygame.display.flip()
+
+pygame.quit()
